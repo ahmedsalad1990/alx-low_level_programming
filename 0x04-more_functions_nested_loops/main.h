13fb32081bbbeb5a1522f450977ebef44df1216e
ahmedsalad1990 <saabirhassan@egmail.com>
2023-03-24 04:24:48 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
+
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,4 +19,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif
+
+#endif /* MAIN_H */
